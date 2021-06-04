783518b5e76fe23a9a231aef8540373d3cb04c37
Nguyenvantuanadmin <85373218+Nguyenvantuanadmin@users.noreply.github.com>
2021-06-05 05:18:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100013709941478' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/kemcheese25' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
