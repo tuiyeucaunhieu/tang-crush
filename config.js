f402b93f9a9f787df96efbdeec8cb062582f5b58
Nguyenvantuanadmin <85373218+Nguyenvantuanadmin@users.noreply.github.com>
2021-06-06 03:16:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, chỉ là bạn thôi  :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'mãi yêu cậu thôi hihi <33333333',
     mess: 'Anh biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
